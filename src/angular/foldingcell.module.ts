180bd7f3539b6e4a4d431ac4b642b4994b143466
pandabuilt <leftlanepanda@gmail.com>
2020-04-30 11:17:44 -0400

    cleanup

--- src/angular/foldingcell.module.ts
+++ src/angular/foldingcell.module.ts
@@ -2,12 +2,12 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { FoldingListViewComponent } from './foldingcell.directive';
 import { CommonModule } from '@angular/common';
 
-
 @NgModule({
     declarations: [FoldingListViewComponent],
     imports: [CommonModule],
     exports: [FoldingListViewComponent],
     schemas: [NO_ERRORS_SCHEMA]
 })
+
 export class FoldingListViewModule {
 }
